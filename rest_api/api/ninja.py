c804d326782e495526688089edabe603de94b03f
Filipe André <flipeandre12@gmail.com>
2023-08-13 20:09:08 -0300

    chore: melhora organização

---
+++ rest_api/api/ninja.py
@@ -0,0 +1,26 @@
+from ninja import NinjaAPI
+from django.core import serializers
+from ninja.security import HttpBearer
+from api.models import Livro
+from api.schemas import LivroSchema
+
+
+class GlobalAuth(HttpBearer):
+    def authenticate(self, request, token):
+        if token == "supersecret":
+            return token
+
+api = NinjaAPI(auth=GlobalAuth())
+
+@api.get('livro/')
+def livro(request):
+    livros = Livro.objects.all()
+    json_data = serializers.serialize('json', livros)
+    return {"livros": json_data}
+
+@api.post('/livro/create', response=LivroSchema)
+def create_livro(request, livro: LivroSchema):
+    dict = livro.dict()
+    novo_livro = Livro(**dict)
+    novo_livro.save()
+    return livro
--- rest_api/rest_api/urls.py
+++ rest_api/rest_api/urls.py
@@ -1,30 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
-from ninja import NinjaAPI
-from ninja.security import HttpBearer
-from api.models import Livro
-from api.schemas import LivroSchema
-from django.core import serializers
-
-class GlobalAuth(HttpBearer):
-    def authenticate(self, request, token):
-        if token == "supersecret":
-            return token
-
-api = NinjaAPI(auth=GlobalAuth())
-
-@api.get('livro/')
-def livro(request):
-    livros = Livro.objects.all()
-    json_data = serializers.serialize('json', livros)
-    return {"livros": json_data}
-
-@api.post('/livro/create', response=LivroSchema)
-def create_livro(request, livro: LivroSchema):
-    dict = livro.dict()
-    novo_livro = Livro(**dict)
-    novo_livro.save()
-    return livro
+from django.urls import path
+from api.ninja import api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
