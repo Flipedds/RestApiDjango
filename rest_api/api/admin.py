2a1f0e34b7bd564159e9c5453270e7f573214155
Filipe André <flipeandre12@gmail.com>
2023-08-24 10:10:27 -0300

    refactor: files

--- rest_api/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from api.models import Livro
-
-
-admin.site.register(Livro)
-
-# Register your models here.
--- rest_api/api/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Livro(models.Model):
-    titulo = models.CharField(max_length=100)
-    autor = models.CharField(max_length=40)
-    editora = models.CharField(max_length=40)
